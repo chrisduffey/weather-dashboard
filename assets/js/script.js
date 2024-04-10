4c614e20fbbe37c84861eec319016abacbe88d7c
chrisduffey <duffeyvt12@gmail.com>
2024-04-10 18:32:21 -0400

    java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,49 @@
 // global variable
-var apiKey = 'ecd683ba8b864d9192d530fdce8e639c';
-var savedLocations = [];
+const apiKey = 'ecd683ba8b864d9192d530fdce8e639c';
+let savedLocations = [];
 
 
 // searched locations
-var searchHistory = function(cityName) {
-$('.recent-search:contains("' + cityName + '")').remove();
+const recentSearchs = document.querySelector('.recent-search');
+recentSearchs.forEach(element => {
+    if (element.textContent.includes(cityName)) {
+        element.parentNode.removeChild(element);
+    }
+});
 
 
 // city input
-var searchHistoryInput = $("<p>");
-searchHistoryInput.addClass("recent-search");
-searchHistoryInput.text(cityName);
+const searchHistoryInput = document.createElement("p");
+searchHistoryInput.classList.add("recent-search");
+searchHistoryInput.textContent = cityName;
 
 // create container for searches
-var searchContainer = $("<div>");
-searchContainer.addClass("recent-search-container");
+const searchContainerEL = document.getElementById('recent-search-container');
+const searchContainer = document.createElement('div');
+searchContainer.classList.add('recent-search-container');
+searchContainer.appendChild(searchHistoryInput);
+searchContainerEL.appendChild(searchContainer);
 
-// append search container
-searchContainer.append(searchHistoryInput);
 
-// append inputs to the container
-var searchContainerEl = $("#recent-search-container");
-searchContainerEl.append(searchContainer);
-
-// if statement
 if (savedLocations.length > 0) {
-    var recentSavedLocations = localStorage.getItem("savedLocations");
-    savedLocations = JSON.parse(recentSavedLocations);
+    const recentSavedLocations = localStorage.getItem("savedLocations");
+    savedLocations = json.parse(recentSavedLocations);
 }
+
+
 
 // add city to array
 savedLocations.push(cityName);
 localStorage.setItem("savedLocations", JSON.stringify(savedLocations));
 
 // reset search
-$("#search-input").val("");
+document.getElementById("search-input").value = "";
 
-};
+
 
 // put saved location in history div
-var loadSearchLocation = function() {
-    var savedLoctionHisotry = localStorage.getItem("savedLocations");
+const loadSearchLocation = () => {
+    let savedLoctionHisotry = localStorage.getItem("savedLocations");
 
     // if statement for no prevoise saved location
     if (!savedLoctionHisotry) {
@@ -55,16 +57,61 @@ var loadSearchLocation = function() {
     }
 };
 
-var currentWeatherSection = function(cityname) {
+const currentWeatherSection = (cityName) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(response){
-        var cityLon = response.coord.lon;
-        var cityLat = response.coord.lat;
+        .then(response => response.json())
+        .then(response => {
+            var cityLon = response.coord.lon;
+            var cityLat = response.coord.lat;
 
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${APIkey}`)
-    })
-    
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${APIkey}`)
+
+        })
+        .then(response => response.json())
+        .then(response => {
+            searchHistoryList(cityName);
+
+            const currentWeatherCont = document.getElementById("current-weather-cont");
+            currentWeatherCont = classList.add("current-weather-cont");
+
+            let currentTitle = document.getElementById("current-header");
+            let currentDay = moment().format("MM/DD/YYYY");
+            currentTitle.textContent = `${cityName} (${currentDay})`;
+
+            let currentIcon = document.getElementById("current-weather-icon");
+            currentIcon.classList.add("current-weather-icon");
+            let currentIconCode = response.current.weather[0].icon;
+            currentIcon.setAttribute("src", `https://openweathermap.org/img/wn/${currentIconCode}@2x.png`);
+
+            let currentTemperature = document.getElementById("current-temperature");
+            currentTemperature.textContent = "Temperature: " + response.current.temp + " \u00B0F";
+
+            let currentHumidity = document.getElementById("current-humidity");
+            currentHumidity.textContent = "Humidity: " + response.current.humidity + "%";
+
+            let currentWindSpeed = document.getElementById("current-wind-speed");
+            currentWindSpeed.textContent = "Wind Speed: " + response.current.wind_speed + "MPH";
+
+            let currentUvIndex = document.getElementById("currnet-uv-index");
+            currentUvIndex.textContent = "UV: " + response.current.uvi;
+
+        })
+        .catch(function (error) {
+            console.error('Error fetching data:', error);
+        });
+};
+
+const fiveDayForecastSection = (cityName) =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+
+    .then 
 }
+
+
+         
+                
+
+
+
+
+
